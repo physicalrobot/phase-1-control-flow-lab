b2004f98fc6c7522239e00bcb28ffa7f5893eaf8
Vikalp Malhotra <kimboho@gmail.com>
2021-12-13 13:19:27 -0500

    finished

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance) {
+
+  let sample = 'This one is on me!'
+  let charge = 'I will gladly take your thirty bucks.'
+  let goaway = 'No can do.'
   // Write your code here!
+
+  if (distance <= 400) {
+    return sample
+  } else if (distance > 400 && distance < 2500) {
+    return charge;
+  } else if (distance > 2500) {
+
+    return goaway
+  }
+
+
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+
+  const citycheck = city == 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return citycheck;
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
+
   // Write your code here!
+  switch (tip) {
+    case "generous":
+      return 'Thank you so much.';
+      break;
+
+    case "not as generous":
+      return 'Thank you.';
+      break;
+
+    default:
+      return 'Bye.';
+  }
+
 }
